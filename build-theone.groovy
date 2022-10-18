0b3a24d96035e107ce03ec7a4084c64acaba676a
samsu0202 <36395475+samsu0202@users.noreply.github.com>
2022-10-18 13:13:37 +0800

    Create build-theone.groovy

---
+++ build-theone.groovy
@@ -0,0 +1,18 @@
+pipeline {
+  agent: any
+  stages {
+    stage("Clone source") {
+      steps {
+        dir('source') {
+          git credentialsId: 'vault-git-sdd-aws-acuity', branch:'vssd', url: 'https://sdd-gitlab.vivotek.tw/aws-acuity/theone.git'
+          // replace submodules from ssh to https because we use deploy token for CI
+          sh """
+            sed -i 's/git@/https:\/\//g;s/:aws-acuity/\/aws-acuity/g' .gitmodules
+            git submodule sync
+            git submodule update --init --recursive
+          """
+        }
+      }
+    }
+  }
+}
