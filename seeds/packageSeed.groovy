99260ac80f9f25f462c3375d7d0b1140e9a46283
samsu0202 <36395475+samsu0202@users.noreply.github.com>
2022-10-20 17:06:31 +0800

    Update packageSeed.groovy

--- seeds/packageSeed.groovy
+++ seeds/packageSeed.groovy
@@ -6,14 +6,11 @@ folder('packages') {
 PLATFORMS.each { platform ->
     pipelineJob("packages/Theone-$platform") {
         definition {
-//             cps {
-//                 // add "this" to fix "Scripts not permitted"
-//                 // https://issues.jenkins.io/browse/JENKINS-45778
-//                 script(this.readFileFromWorkspace('pipelines/build-theone.groovy'))
-//                 sandbox()
-//             }
-            cpsScm {
-                scriptPath('pipelines/build-theone.groovy')
+            cps {
+                // add "this" to fix "Scripts not permitted"
+                // https://issues.jenkins.io/browse/JENKINS-45778
+                script(this.readFileFromWorkspace('pipelines/build-theone.groovy'))
+                sandbox()
             }
         }
         parameters {
