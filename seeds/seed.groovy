793e7cc6b5433419969a04cdf46e98b2439f0a11
samsu0202 <36395475+samsu0202@users.noreply.github.com>
2022-10-20 14:10:46 +0800

    Update seed.groovy

--- seeds/seed.groovy
+++ seeds/seed.groovy
@@ -5,7 +5,7 @@ pipeline {
     stages {
         stage('Pipeline to seed or update all pipelines') {
             steps {
-                jobDsl targets: ['packageSeed.groovy'].join('\n'),
+                jobDsl targets: ['packageSeed1.groovy'].join('\n'),
                        removedJobAction: 'DELETE',
                        removedViewAction: 'DELETE',
                        lookupStrategy: 'SEED_JOB'
