d80edfd21791476c641a2fb42c931aefc9ba1c8c
samsu0202 <36395475+samsu0202@users.noreply.github.com>
2022-10-20 10:40:44 +0800

    Create seed.groovy

---
+++ seed.groovy
@@ -0,0 +1,11 @@
+pipeline {
+    agent any 
+    stages {
+        stage('Pipeline to seed or update all pipelines') {
+            steps {
+                // jobDsl  targets: ['*.groovy'].join('\n')
+                echo 123
+            }
+        }
+    }
+}
