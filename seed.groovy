5e8adb9136a06796853667c67fb716e28df3d7f4
samsu0202 <36395475+samsu0202@users.noreply.github.com>
2022-10-20 10:49:30 +0800

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -1,10 +1,17 @@
+def VSaaSPlatforms = ['Amba', 'SStar']
+
 pipeline {
     agent any 
     stages {
         stage('Pipeline to seed or update all pipelines') {
             steps {
+                scripts {
+                    $VSaaSPlatforms.each { platform ->
+                        echo $platform
+                    }
+                }
                 // jobDsl  targets: ['*.groovy'].join('\n')
-                echo 123
+//                 echo 123
             }
         }
     }
