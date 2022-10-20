f00121d756b46d5431e824b04b03967d346e1789
samsu0202 <36395475+samsu0202@users.noreply.github.com>
2022-10-20 13:43:08 +0800

    Update packageSeed.groovy

--- seeds/packageSeed.groovy
+++ seeds/packageSeed.groovy
@@ -1,1 +1,24 @@
-
+final PLATFORMS = ['Amba', 'SStar']
+folder('packages') {
+    displayName('Packages')
+    description('Folder for packages')
+}
+PLATFORMS.each { platform ->
+    pipelineJob("packages/Theone-$platform-test") {
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            github('samsu0202/pipeline-scripts')
+                        }
+                    }
+                }
+                scriptPath('build-theone.groovy')
+            }
+        }
+        parameters {
+            stringParam('platform', "$platform", "$platform")
+        }
+    }
+}
